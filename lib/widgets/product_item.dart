96a219f86d47b4a1c2def01c4c51e74daace0076
Pannakoek <k.vnarkel@live.nl>
2021-08-25 17:45:21 +0200

    Allow the user to undo add actions to the card

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -46,6 +46,19 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItems(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Added item to cart!"),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
